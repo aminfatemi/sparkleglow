174f76d8ad1f9c614f2e3e0ae9fbadd921a447eb
Amin <jjxamin@gmail.com>
2017-06-05 20:24:21 +0200

    artisan migrate string error, fix defaultStringLength(191), debugging verbosity

--- database/migrations/2017_06_05_172454_create_posts_table.php
+++ database/migrations/2017_06_05_172454_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $title->mediumText('body');
+            $table->mediumText('body');
             $table->timestamps();
         });
     }
